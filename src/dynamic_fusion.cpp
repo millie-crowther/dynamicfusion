b53c83b8b6178f84438f2735a3d204d84bf444c8
millie <mbc15@ic.ac.uk>
2018-02-18 19:15:51 +0000

    hello world example working with ceres

--- src/dynamic_fusion.cpp
+++ src/dynamic_fusion.cpp
@@ -5,6 +5,13 @@
 
 using namespace ceres;
 
+struct CostFunctor {
+  template <typename T> bool operator()(const T* const x, T* residual) const {
+    residual[0] = 10.0 - x[0];
+    return true;
+  }
+};
+
 dynamic_fusion_t::dynamic_fusion_t(){
 
 }
@@ -15,5 +22,28 @@ dynamic_fusion_t::~dynamic_fusion_t(){
 
 void
 dynamic_fusion_t::execute(){
-    Problem problem;
+  // The variable to solve for with its initial value. It will be
+  // mutated in place by the solver.
+  double x = 0.5;
+  const double initial_x = x;
+
+  // Build the problem.
+  Problem problem;
+
+  // Set up the only cost function (also known as residual). This uses
+  // auto-differentiation to obtain the derivative (jacobian).
+  CostFunction* cost_function =
+      new AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
+  problem.AddResidualBlock(cost_function, NULL, &x);
+
+  // Run the solver!
+  Solver::Options options;
+  options.minimizer_progress_to_stdout = true;
+  Solver::Summary summary;
+  Solve(options, &problem, &summary);
+
+  std::cout << summary.BriefReport() << "\n";
+  std::cout << "x : " << initial_x
+            << " -> " << x << "\n";
+
 }
